c7a976356dab05ffde0909e24c29a1e59c92f0cf
Yub Maharjan <yubmaharjan322@gmail.com>
2024-01-17 14:19:52 +0545

    static webpage for python traineeship

--- js/chunk-vendors~d4c9f4a7.572d2b9d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~d4c9f4a7"],{f9e3:function(n,o,w){}}]);
-//# sourceMappingURL=chunk-vendors~d4c9f4a7.572d2b9d.js.map
--- js/chunk-vendors~f9862c3f.4131504c.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~f9862c3f"],{"2dd8":function(n,o,w){}}]);
-//# sourceMappingURL=chunk-vendors~f9862c3f.4131504c.js.map
